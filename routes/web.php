fd18fd3fdd3e5abc61d767347c31216d6bb83d0d
Brian Revie <brian@nmcreative.co.uk>
2021-10-28 09:57:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/noaccess', NoaccessController::class)->name('noaccess');
 
 Route::group(
     [
-        'middleware' => ['auth', 'is_admin'], 
+        'middleware' => ['auth', ''], 
     ],
     function () {
         // Main Dashboard
