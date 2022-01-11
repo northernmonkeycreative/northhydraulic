5c2844e8d422c38d9051cd87cac2331efafacbe0
Brian Revie <brian@nmcreative.co.uk>
2022-01-11 14:56:49 +0000

    Update show.blade.php

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -133,7 +133,7 @@
 
 
 
-                                {{-- <span>
+                                <span>
                                     <span class="sm:ml-3">
                                         <a href="{{ route('jobs.edit', $thejob->id) }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                             <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -142,7 +142,7 @@
                                             Edit This Job
                                         </a>
                                     </span> 
-                                </span> --}}
+                                </span>
                            
                             </div>
                                 
