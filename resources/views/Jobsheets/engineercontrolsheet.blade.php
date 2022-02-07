a5c292ea6e42c0defabd857f9be52eb9c75bc01b
Brian Revie <brian@nmcreative.co.uk>
2022-02-07 11:48:39 +0000

    Update engineercontrolsheet.blade.php

--- resources/views/Jobsheets/engineercontrolsheet.blade.php
+++ resources/views/Jobsheets/engineercontrolsheet.blade.php
@@ -157,7 +157,7 @@
 
                                     <div class="bg-gray-50 px-4 py-3 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                                         <dt class="text-sm font-medium text-gray-500">
-                                            Customer Signature
+                                            Customer Signatures
                                         </dt>
                                         <img id="" src="{{$customersignature}}" width="auto">
                                     </div>
