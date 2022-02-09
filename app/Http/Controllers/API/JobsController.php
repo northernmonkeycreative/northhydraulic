1d2ccfdb33d2c243b9bdacb1fa65e1e5e7a74c36
Brian Revie <brian@nmcreative.co.uk>
2022-02-09 13:56:29 +0000

    Update JobsController.php

--- app/Http/Controllers/API/JobsController.php
+++ app/Http/Controllers/API/JobsController.php
@@ -22,7 +22,7 @@ class JobsController extends Controller
     // get single engineer control sheet
     public function uploadimages(Request $request)
     {
-        return Response::json(stringify($request));
+        return Response::json($request);
         $images = $request->images;
         $jobid = $request->job_id;
 
