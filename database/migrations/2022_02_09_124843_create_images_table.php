b48f47c44baa18f8e8be339d537a43ecdff5e9b5
Brian Revie <brian@nmcreative.co.uk>
2022-02-09 12:54:05 +0000

    Create 2022_02_09_124843_create_images_table.php

---
+++ database/migrations/2022_02_09_124843_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('job_id')->unsigned()->index();
+            $table->longText('image')->nullable();
+            $table->timestamps();
+        });
+        Schema::table('images', function($table) {
+            $table->foreign('job_id')->references('id')->on('jobs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
