c76385588d7768de35f7c2e29c62912241d8bf40
Brian Revie <brian@nmcreative.co.uk>
2022-02-10 11:51:29 +0000

    Update show.blade.php

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -459,7 +459,7 @@
                                                 
                                                     <div class="group relative">
                                                             <div class="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                                                <a data-remote="{{$image->image}}" data-toggle="lightbox">
+                                                                <a data-src="{{$image->image}}" data-toggle="lightbox">
                                                                     <img id="" src="{{$image->image}}" class="w-full h-full object-center object-cover lg:w-full lg:h-full">
                                                                 </a>
                                                             </div>
