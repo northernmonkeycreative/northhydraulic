2b1d67c4684c35697d39f9560376e102c2d7fe79
Brian Revie <brian@nmcreative.co.uk>
2022-02-10 11:15:50 +0000

    Update show.blade.php

--- resources/views/Jobs/show.blade.php
+++ resources/views/Jobs/show.blade.php
@@ -448,10 +448,8 @@
                                     <!-- Images -->
                                     <hr>
                                     <div class="bg-white">
-                                        <dt class="text-sm font-medium text-gray-500">
-                                            Images Uploaded To This Job
-                                        </dt>
-                                        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">                                      
+                                        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">   
+                                                <h2 class="text font-extrabold tracking-tight text-gray-900">Images Uploaded To This Job</h2>                                   
                                             <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                                                 @foreach($images as $image)
                                                 <div class="group relative">
