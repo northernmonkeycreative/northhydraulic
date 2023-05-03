30c1592177bf2359f687179be4ee06015c0922d1
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-03 10:49:42 +0100

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Jobs Calendar') }}
+                {{ __('Jobs Calendars') }}
             </h2>
 
             <div>
