b5b874fbbe327ac355d6c09d34d3777804a3a23b
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-04 09:00:11 +0100

    Update jobs.blade.php

--- resources/views/Jobs/jobs.blade.php
+++ resources/views/Jobs/jobs.blade.php
@@ -113,9 +113,30 @@
                                                 @endif
                                                 @if($job->status == 'Invoiced')
                                                 <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-white-500 text-gray-500 uppercase border border-gray-300">Invoiced</span>
-                                                @endif
-                                                @if($job->internal_notes)
-                                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-white-500 text-gray-500 uppercase border border-gray-300">{{$job->internal_notes}}</span>
+                                                
+                                               
+                                                <span class="px-2 inline-flex text-xs leading-5 text-gray-500">
+                                                    <span>
+                                                        @if($job->internal_notes)
+                                                            <span class="note">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 019 9v.375M10.125 2.25A3.375 3.375 0 0113.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 013.375 3.375M9 15l2.25 2.25L15 12" />
+                                                                </svg>
+                                                            </span>
+                                                        @endif
+                                                    </span>
+                                                    <span class="internalnotes hidden">
+                                                        {{$job->internal_notes}}
+                                                    </span>
+                                                    <style>
+                                                        .note:hover .internalnotes {
+                                                            display:block;
+                                                        }
+                                                        .internalnotes {
+                                                            display:none;
+                                                        }
+                                                    </style>
+                                                </span>
                                                 @endif
                                             </td>
                                             @if (request()->is('jobs/paid'))
