c50c369fb078af875c62b59b8f311e8e9b62b167
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-04 09:33:40 +0100

    Update jobs.blade.php

--- resources/views/Jobs/jobs.blade.php
+++ resources/views/Jobs/jobs.blade.php
@@ -126,7 +126,7 @@
                                                             </span>
                                                         @endif
                                                         @if($job->internal_notes == "")
-                                                            <span class="note px-2 py-2 text-sm font-medium text-gray-700">
+                                                            <span class="note px-2 py-2 text-sm font-medium text-gray-200">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                                 </svg>
