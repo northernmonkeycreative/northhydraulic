29968abc43c7dd32e74307137d433c52795d4735
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-04 08:32:11 +0100

    Update jobs.blade.php

--- resources/views/Jobs/jobs.blade.php
+++ resources/views/Jobs/jobs.blade.php
@@ -116,7 +116,7 @@
                                             @if (request()->is('jobs/paid'))
                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$job->invoice_number}}</td>
                                             @endif
-                                            <td class="py-4 inline-flex items-center">
+                                            <td class="inline-flex items-center">
                                                 <a href="{{ route('jobs.show', $job->id) }}"  title="View This Job" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                                     <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500"xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
