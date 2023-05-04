0d97ff7aa999a0d0edb81249ba5ead348c9c25bf
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-04 11:27:53 +0100

    Update index.blade.php

--- resources/views/Clients/index.blade.php
+++ resources/views/Clients/index.blade.php
@@ -68,14 +68,13 @@
                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"><a href="mailto:{{$client->contact_email}}" title="Send Email To {{$client->company_name }}">{{$client->contact_email}}</a></td>
                                             <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{{$client->postcode}}</td>
                                             <td class="py-4">
-                                                    <a href="{{ route('clients.show', $client->id) }}"  class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">
-                                                            <!-- Heroicon name: solid/link -->
-                                                            <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500"xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                                            </svg>
-                                                       
-                                                        </a>
+                                                <a href="{{ route('clients.show', $client->id) }}"  class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">
+                                                    <!-- Heroicon name: solid/link -->
+                                                    <svg class="-ml-1 mr-2 h-5 w-5 text-gray-500"xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                    </svg>
+                                                </a>
                                             </td>
                                         </tr>
                                         @empty
