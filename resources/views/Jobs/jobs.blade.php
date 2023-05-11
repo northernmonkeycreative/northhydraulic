9bab103f3097613c12b1cd6185661272a7587617
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-05-11 09:54:57 +0100

    Update jobs.blade.php

--- resources/views/Jobs/jobs.blade.php
+++ resources/views/Jobs/jobs.blade.php
@@ -218,12 +218,7 @@
                         stateSave: true, //statesave to help keep pagination state
                         stateDuration:-1,
                         responsive: true,
-            processing: true,
-            serverSide: true,
-            ajax: $.fn.dataTable.pipeline({
-               
-                pages: 20 // number of pages
-            }),
+ 
                         stateSaveCallback: function(settings,data) {
                             localStorage.setItem( 'DataTables_' + settings.sInstance, JSON.stringify(data) )
                         },
