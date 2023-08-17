827b83a27540675a69df8ffbe0d06fa23711f5bc
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-17 20:52:31 +0100

    Update paid.blade.php

--- resources/views/jobs/paid.blade.php
+++ resources/views/jobs/paid.blade.php
@@ -55,7 +55,7 @@
                           <div class=" p-4">
                                 <table class="table table-hover min-w-full divide-y divide-gray-200" id="dataTable" width="100%" cellspacing="0">
                                     <thead class="bg-gray-50">
-                                    <tr>
+                                    <!-- <tr>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Job Number</th>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Customer</th>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vehicle Reg</th>
@@ -68,10 +68,10 @@
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Invoice Number</th>
                                             
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                                        </tr>
+                                        </tr> -->
                                     </thead>
                                     <tfoot class="bg-gray-50">
-                                        <tr>
+                                        <!-- <tr>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Job Number</th>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Customer</th>
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Vehicle Reg</th>
@@ -84,7 +84,7 @@
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Invoice Number</th>
                                         
                                             <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                                        </tr>
+                                        </tr> -->
                                     </tfoot>
                                     <tbody>
                                        
