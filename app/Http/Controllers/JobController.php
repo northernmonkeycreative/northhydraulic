7562be41d8d6218bb5555a0b7294d60c1220e3fe
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-17 19:22:21 +0100

    Update JobController.php

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -23,7 +23,7 @@ class JobController extends Controller
         return view('jobs.jobs', compact('jobs'));
     }
 
-    public function getpaidJobs()
+    public function getpaidJobs(Request $request)
     {
 
         if ($request->ajax()) {
