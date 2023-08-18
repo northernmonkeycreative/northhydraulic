151b4823ab7da6947ae20a831edd2b0713086e0c
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-18 19:58:01 +0100

    Update paid.blade.php

--- resources/views/jobs/paid.blade.php
+++ resources/views/jobs/paid.blade.php
@@ -145,8 +145,8 @@
                             }
                             
                         ],
-                        // stateSave: false, //statesave to help keep pagination state
-                        // stateDuration:-1,
+                        stateSave: false, //statesave to help keep pagination state
+                        stateDuration:-1,
                         stateSaveCallback: function(settings,data) {
                             localStorage.setItem( 'DataTables_' + settings.sInstance, JSON.stringify(data) )
                         },
