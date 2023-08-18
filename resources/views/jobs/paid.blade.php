89190e52c2651f3ed5c8e8a626a7282b4a226dbb
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-18 20:24:13 +0100

    Update paid.blade.php

--- resources/views/jobs/paid.blade.php
+++ resources/views/jobs/paid.blade.php
@@ -94,74 +94,68 @@
 
         <script>
 
-            (function($) {
-                "use strict";
+(function($) {
+    "use strict";
 
-                // Call the dataTables jQuery plugin
-                $(document).ready(function() {
+    // Call the dataTables jQuery plugin
+    $(document).ready(function() {
 
-                    console.log("DataTables initialization script is running.");
+        console.log("DataTables initialization script is running.");
 
+        $('#dataTable').DataTable({
+            processing: true,
+            serverSide: true,
+            paging: true,
+            ajax: "{{ route('jobs.paid') }}",
+            success: function(response) {
+                console.log("Fetched data:", response);
+            },
+            columns: [
+                { data: 'id', name: 'id', "className": "py-4 py-4 font-bold" },
+                { data: 'customer_name', name: 'customer_name', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'department', name: 'department', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'start_date', name: 'start_date', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'reg', name: 'reg', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'invoice_number', name: 'invoice_number', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'engineer_name', name: 'engineer_name', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                { data: 'status', name: 'status', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
+                
+                // Action buttons
+                // Add the view and edit buttons as additional columns
+                {
+                    "data": null,
+                    "className": "py-4 inline-flex items-center",
+                    "render": function(data, type, row) {
+                        return '<a href="' + row.view_url + '" title="View This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
+                            '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">' +
+                                '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>' +
+                                '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>' +
+                            '</svg>' +
+                            '</a>' +
+                            '<a href="' + row.edit_url + '" title="Edit This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
+                            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">' +
+                                '<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"></path>' +
+                            '</svg>' +
+                            '</a>';
+                    }
+                }
+            ],
+            stateSave: false, //statesave to help keep pagination state
+            stateDuration: -1,
+            stateSaveCallback: function(settings, data) {
+                localStorage.setItem('DataTables_' + settings.sInstance, JSON.stringify(data));
+            },
+            stateLoadCallback: function(settings) {
+                return JSON.parse(localStorage.getItem('DataTables_' + settings.sInstance));
+            },
+            "order": [[0, 'desc']]
+        });
 
-                    $('#dataTable').DataTable({
-                        processing: true,
-                        serverSide: true,
-                        paging: true,
-                        ajax: "{{ route('jobs.paid') }}",
-                        success: function(response) {
-                            console.log("Fetched data:", response);
-                        },
-                        columns: [
-                            { data: 'id', name: 'id', "className": "py-4 py-4 font-bold" },
-                            { data: 'customer_name', name: 'customer_name', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'department', name: 'department', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'start_date', name: 'start_date', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'reg', name: 'reg', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'invoice_number', name: 'invoice_number', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'engineer_name', name: 'engineer_name', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            { data: 'status', name: 'status', "className": "px-6 py-4 whitespace-nowrap text-sm text-gray-500" },
-                            
-                            // action buttons
-                            // Add the view and edit buttons as additional columns
-                            // {
-                            //     "data": null,
-                            //     "className": "py-4 inline-flex items-center",
-                            //     "render": function(data, type, row) {
-                            //         console.log("Rendering action buttons for row:", row);
-                            //         console.log("View URL:", row.view_url);
-                            //         console.log("Edit URL:", row.edit_url);
-                            //         return '<a href="' + row.view_url + '" title="View This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
-                            //             '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">' +
-                            //                 '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />' +
-                            //                 '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />' +
-                            //             '</svg>' +
-                            //             '</a>' +
-                            //             '<a href="' + row.edit_url + '" title="Edit This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
-                            //             '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">' +
-                            //                 '<path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />' +
-                            //             '</svg>' +
-                            //             '</a>';
-                            //     }
-                            // }
-                            
-                        ],
-                        stateSave: false, //statesave to help keep pagination state
-                        stateDuration:-1,
-                        stateSaveCallback: function(settings,data) {
-                            localStorage.setItem( 'DataTables_' + settings.sInstance, JSON.stringify(data) )
-                        },
-                        stateLoadCallback: function(settings) {
-                            return JSON.parse( localStorage.getItem( 'DataTables_' + settings.sInstance ) )
-                        },
-                        "order": [[ 0, 'desc' ]]
-                    });
+        console.log('datatable is loaded');
+    });
 
-                    console.log('databatble is loaded')
-                });
+})(jQuery);
 
-
-
-            })(jQuery);
 
 
             </script>
