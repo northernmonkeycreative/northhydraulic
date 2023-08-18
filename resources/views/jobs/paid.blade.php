d5b0963c45610b10fef5eb0419011a0be31daecd
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-18 20:09:47 +0100

    Update paid.blade.php

--- resources/views/jobs/paid.blade.php
+++ resources/views/jobs/paid.blade.php
@@ -130,6 +130,9 @@
                                 "data": null,
                                 "className": "py-4 inline-flex items-center",
                                 "render": function(data, type, row) {
+                                    console.log("Rendering action buttons for row:", row);
+                                    console.log("View URL:", row.view_url);
+                                    console.log("Edit URL:", row.edit_url);
                                     return '<a href="' + row.view_url + '" title="View This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
                                         '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">' +
                                             '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />' +
