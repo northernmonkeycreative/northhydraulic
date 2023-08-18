6e4fef89633027d937528667cf3820bdf4e8585b
Northern Monkey Creative Media <brian@nmcreative.co.uk>
2023-08-18 20:45:56 +0100

    Update paid.blade.php

--- resources/views/jobs/paid.blade.php
+++ resources/views/jobs/paid.blade.php
@@ -126,7 +126,7 @@
                 // Add the view and edit buttons as additional columns
                 {
                     data: null,
-                    "className": "py-4 inline-flex items-center",
+                    "className": "",
                     "render": function(data, type, row) {
                         return '<a href="' + row.view_url + '" title="View This Job" class="inline-flex items-center px-2 py-2 text-sm font-medium text-gray-700">' +
                             '<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">' +
